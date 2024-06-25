1b4c12178607c823a280bbba0dfea8ec31a162c1
ZeEE <874074406@qq.com>
2024-06-25 20:57:24 +0800

    目前在app可以printf到terminal

--- SwiftTCP/ContentView.swift
+++ SwiftTCP/ContentView.swift
@@ -7,18 +7,58 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct indexView: View {
+    
+    @State private var inputText: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack(spacing: 20) {
+                
+                
+                TextField("请输入内容", text: $inputText)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding()
+                            
+                            Button(action: {
+                                print("输入的内容是：\(self.inputText)")
+                            }) {
+                                Text("打印输入内容")
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .background(Color.blue)
+                                    .cornerRadius(8)
+                            }
+
+                Button(action: {
+                    // 按钮被点击时的操作
+                    print("Sun button tapped")
+                }) {
+                    Image(systemName: "sun.max.fill")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                }
+                
+                Button(action: {
+                    // 按钮被点击时的操作
+                    print("Folder button tapped")
+                }) {
+                    Image(systemName: "folder.fill")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                }
+                
+                Button(action: {
+                    // 按钮被点击时的操作
+                    print("Person button tapped")
+                }) {
+                    Image(systemName: "person.crop.circle")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                }
+            }
+            .padding()
         }
-        .padding()
-    }
 }
 
 #Preview {
-    ContentView()
+    indexView()
 }
--- SwiftTCP/SwiftTCPApp.swift
+++ SwiftTCP/SwiftTCPApp.swift
@@ -1,17 +1,10 @@
-//
-//  SwiftTCPApp.swift
-//  SwiftTCP
-//
-//  Created by 何金泽 on 2024/6/25.
-//
-
 import SwiftUI
 
 @main
-struct SwiftTCPApp: App {
+struct MySwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            indexView()
         }
     }
 }
