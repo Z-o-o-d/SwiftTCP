6434efcb138fcbef85b8f0b5b17d78146726b5fb
ZeEE <874074406@qq.com>
2024-06-25 19:47:01 +0800

    Initial Commit

---
+++ SwiftTCP/SwiftTCPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTCPApp.swift
+//  SwiftTCP
+//
+//  Created by 何金泽 on 2024/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTCPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
